4284ba1369677953e63f952d1c1250714536fa82
Noelia Ruiz Martínez <nrm1977@gmail.com>
2016-06-15 11:40:25 +0200

    Updated readme and add-on summary

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# reportPasswords
-NVDA add-on to add the option of reporting typed characters on protected controls as passwords.
+  # Report Passwords #
+* Author: Noelia Ruiz Martínez.
+
+This add-on adds the option of speaking the typed text on protected controls as passwords.
+
+## Key commands ##
+* nvda+control+shift+p: Toggles on and off the speaking of text typed on protected controls as passwords.
+
+Note: The above command can be changed from NVDA menu, Preferences submenu, Input gestures dialog, Speech category.
+
+## Preferences Menu ##
+* Report Passwords settings: Allows to toggle on and off the speaking of text typed on protected controls as passwords.
+
+Note: The above command can be changed from NVDA menu, Preferences submenu, Input gestures dialog, Configuration category.
+
+## Changes for 1.0 ##
+* Initial version.
---
+++ addon/globalPlugins/reportPasswords.py
@@ -0,0 +1,98 @@
+# reportPasswords: plugin to avoid keyboard echo suppressing on protected controls
+# Settings managed with config.conf of NVDA
+# Date: 15/06/2016
+# Initial version
+# Date: 16/04/2016
+
+import addonHandler
+import globalPluginHandler
+import api
+import ui
+import config
+import wx
+import gui
+from gui import SettingsDialog
+from globalCommands import SCRCAT_SPEECH, SCRCAT_CONFIG
+
+addonHandler.initTranslation()
+
+confspec = {
+	"unprotectControls": "boolean(default=False)",
+}
+
+config.conf.spec["reportPasswords"] = confspec
+isTypingProtected = api.isTypingProtected
+
+def unprotectControls():
+	return False
+
+def restoreIsProtected():
+	api.isTypingProtected = isTypingProtected
+
+class AddonSettingsDialog(SettingsDialog):
+
+	# Translators: title of a dialog.
+	title = _("ReportPasswords settings")
+
+	def makeSettings(self, settingsSizer):
+		# Translators: label of a dialog.
+		self.reportPasswordsCheckBox=wx.CheckBox(self, wx.NewId(), label=_("&Report passwords"))
+		self.reportPasswordsCheckBox.SetValue(config.conf["reportPasswords"]["unprotectControls"])
+		settingsSizer.Add(self.reportPasswordsCheckBox,border=10, flag=wx.BOTTOM)
+
+	def postInit(self):
+		self.reportPasswordsCheckBox.SetFocus()
+
+	def onOk(self,evt):
+		config.conf["reportPasswords"]["unprotectControls"] = self.reportPasswordsCheckBox.GetValue()
+		if config.conf["reportPasswords"]["unprotectControls"]:
+			api.isTypingProtected = unprotectControls
+		else:
+			restoreIsProtected()
+		super(AddonSettingsDialog, self).onOk(evt)
+
+class GlobalPlugin(globalPluginHandler.GlobalPlugin):
+
+	def __init__(self):
+		super(globalPluginHandler.GlobalPlugin, self).__init__()
+		if config.conf["reportPasswords"]["unprotectControls"]:
+			api.isTypingProtected = unprotectControls
+		self.prefsMenu = gui.mainFrame.sysTrayIcon.preferencesMenu
+		self.settingsItem = self.prefsMenu.Append(wx.ID_ANY,
+			# Translators: name of the option in the menu.
+			_("&ReportPasswords settings..."),
+			"")
+		gui.mainFrame.sysTrayIcon.Bind(wx.EVT_MENU, self.onSettings, self.settingsItem)
+
+	def terminate(self):
+		if config.conf["reportPasswords"]["unprotectControls"]:
+			restoreIsProtected()
+		try:
+			self.prefsMenu.RemoveItem(self.settingsItem)
+		except wx.PyDeadObjectError:
+			pass
+
+	def onSettings(self, evt):
+		gui.mainFrame._popupSettingsDialog(AddonSettingsDialog)
+
+	def script_settings(self, gesture):
+		wx.CallAfter(self.onSettings, None)
+	script_settings.category = SCRCAT_CONFIG
+	# Translators: message presented in input help mode.
+	script_settings.__doc__ = _("Shows the ReportPasswords settings dialog.")
+
+	def script_toggleReportPasswords(self, gesture):
+		config.conf["reportPasswords"]["unprotectControls"] = not config.conf["reportPasswords"]["unprotectControls"]
+		if config.conf["reportPasswords"]["unprotectControls"]:
+			api.isTypingProtected = unprotectControls
+			ui.message(_("Report passwords on"))
+		else:
+			restoreIsProtected()
+			ui.message(_("Report passwords off"))
+	script_toggleReportPasswords.category = SCRCAT_SPEECH
+	# Translators: message presented in input help mode.
+	script_toggleReportPasswords.__doc__ = _("Toggles on and off the speaking of text typed on protected controls as passwords.")
+
+	__gestures = {
+		"kb:NVDA+control+shift+p": "toggleReportPasswords",
+	}
