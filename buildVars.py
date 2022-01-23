80ad98918a80369355184576f8dc18dfa6a2648d
Noelia Ruiz Martínez <nrm1977@gmail.com>
2022-01-23 16:18:28 +0100

    Update workflow and buildVars

--- buildVars.py
+++ buildVars.py
@@ -23,13 +23,13 @@ addon_info = {
 	# Author(s)
 	"addon_author": u"Noelia Ruiz Martínez <nrm1977@gmail.com>",
 	# URL for the add-on documentation support
-	"addon_url": "https://addons.nvda-project.org/addons/reportPasswords",
+	"addon_url": "https://github.com/nvdaes/reportPasswords",
 	# Documentation file name
 	"addon_docFileName": "readme.html",
 	# Minimum NVDA version supported (e.g. "2018.3")
 	"addon_minimumNVDAVersion": "2019.3",
 	# Last NVDA version supported/tested (e.g. "2018.4", ideally more recent than minimum version)
-	"addon_lastTestedNVDAVersion": "2021.1",
+	"addon_lastTestedNVDAVersion": "2021.3.1",
 	# Add-on update channel (default is stable or None)
 	"addon_updateChannel": None,
 }
